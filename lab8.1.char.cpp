02908723f5a0ad607b1456082bfad489cde21a4e
1gpm1 <tarasianishevskii>
2023-03-27 22:50:44 +0300

    Add project files.

---
+++ lab8.1.char.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int Count(char* str)
+{
+	int k = 0;
+	while (*str != '\0') {
+		if (*str == *(str + 1) && *str == *(str + 2) && *str == *(str + 3)) {
+			k++;
+		}
+		str++;
+	}
+	return k;
+}
+void Change(char* str)
+{
+	while (*str != '\0') {
+		if (*str == *(str + 1) && *str == *(str + 2) && *str == *(str + 3)) {
+			strncpy(str, "**", 2);
+			memmove(str + 2, str + 4, strlen(str + 4) + 1);
+		}
+		str++;
+	}
+}
+int main()
+{
+	char str[101];
+
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+
+	cout << "There is " << Count(str) << " four simular char in line" << endl;
+
+	Change(str);
+
+	cout << str << endl;
+
+	return 0;
+}
